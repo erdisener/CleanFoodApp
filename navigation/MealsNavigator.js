5039692887687c337530f5106df88121224a01b9
erdisener <erdisener@gmail.com>
2019-11-08 10:58:45 +0300

    menu item added

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 import { createStackNavigator } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
 
@@ -5,12 +7,29 @@ import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: {
-    screen: CategoryMealScreen
+import Colors from "../constants/Colors";
+
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: {
+      screen: CategoriesScreen,
+      navigationOptions: {
+        headerTitle: "Meal Categories"
+      }
+    },
+    CategoryMeals: {
+      screen: CategoryMealScreen
+    },
+    MealDetail: MealDetailScreen
   },
-  MealDetail: MealDetailScreen
-});
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "ios" ? Colors.primaryColor : ""
+      },
+      headerTintColor: Platform.OS === "ios" ? "white" : ""
+    }
+  }
+);
 
 export default createAppContainer(MealsNavigator);
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -4,18 +4,18 @@ import {
   Text,
   FlatList,
   StyleSheet,
-  TouchableOpacity,
-  Platform
+  TouchableOpacity
 } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
-import Colors from "../constants/Colors";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 const CategoriesScreen = props => {
   const renderGridItem = itemData => {
     return (
-      <TouchableOpacity
-        style={styles.gridItem}
-        onPress={() => {
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
           props.navigation.navigate({
             routeName: "CategoryMeals",
             params: {
@@ -23,11 +23,7 @@ const CategoriesScreen = props => {
             }
           });
         }}
-      >
-        <View>
-          <Text>{itemData.item.title}</Text>
-        </View>
-      </TouchableOpacity>
+      />
     );
   };
   return (
@@ -41,11 +37,7 @@ const CategoriesScreen = props => {
 };
 
 CategoriesScreen.navigationOptions = {
-  headerTitle: "Meal Categories",
-  headerStyle: {
-    backgroundColor: Platform.OS === "ios" ? Colors.primaryColor : ""
-  },
-  headerTintColor: Platform.OS === "ios" ? "white" : ""
+  headerTitle: "Meal Categories"
 };
 
 const styles = StyleSheet.create({
@@ -53,11 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150
   }
 });
 
