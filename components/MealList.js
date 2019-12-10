643d7f1cbe4a030d5cdebdb15ab188e768cba83f
erdisener <erdisener@gmail.com>
2019-12-11 00:05:39 +0300

    redux applied for favs and filters

--- components/MealList.js
+++ components/MealList.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { FlatList, View, StyleSheet } from "react-native";
 import MealItem from "../components/MealItem";
+import { useSelector } from "react-redux";
 
 const MealList = props => {
+  const favoriteMeals = useSelector(state => state.meals.favoriteMeals);
   const renderMealItem = itemData => {
+    const isFavorite = favoriteMeals.some(meal => meal.id === itemData.item.id);
     return (
       <MealItem
         title={itemData.item.title}
@@ -14,7 +17,11 @@ const MealList = props => {
         onSelectMeal={() => {
           props.navigation.navigate({
             routeName: "MealDetail",
-            params: { mealId: itemData.item.id }
+            params: {
+              mealId: itemData.item.id,
+              mealTitle: itemData.item.title,
+              isFav: isFavorite
+            }
           });
         }}
       />
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,14 +1,26 @@
 import React from "react";
-
-import { CATEGORIES, MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
+import { View, StyleSheet } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealScreen = props => {
   const catId = props.navigation.getParam("categoryId");
 
-  const displayedMeals = MEALS.filter(
+  const availableMeals = useSelector(state => state.meals.filteredMeals);
+
+  const displayedMeals = availableMeals.filter(
     meal => meal.categoryIds.indexOf(catId) >= 0
   );
+
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>Seçilen filtrelere uygun yiyecek bulunamadı!</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
@@ -22,5 +34,13 @@ CategoryMealScreen.navigationOptions = navigationData => {
     headerTitle: selectedCategory.title
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default CategoryMealScreen;
